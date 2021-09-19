de9341f20e83df325e6805922234dae2a967074b
Максим Шаров <sm710mh@gmail.com>
2021-09-20 00:20:26 +0300

    feat: add adaptive

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ if(urlHash.length > 0) {
 var links = document.querySelectorAll('.minu__item-link');
 
 links.forEach(e => {
+  //console.log(window.innerWidth);
   e.addEventListener('click', l => {
     links.forEach(s => {
       if(s.classList.contains('active'));
@@ -15,5 +16,29 @@ links.forEach(e => {
 
     var doActiv = document.querySelector('a[href="' + l.target.hash + '"]');
     doActiv.classList.add('active');
+
+    if(window.innerWidth <= 991) {
+      aside.classList.toggle('burger-out');
+      asideShedow.classList.toggle('visible');
+    }
   })
+})
+
+/* burger */
+var closeBurder = document.querySelector('.close-burger');
+var aside = document.querySelector('aside');
+var asideShedow = document.querySelector('.m_shadow');
+var openBurger = document.querySelector('.m_menu-on');
+
+closeBurder.addEventListener('click', () => {
+  aside.classList.toggle('burger-out');
+  asideShedow.classList.toggle('visible');
+})
+asideShedow.addEventListener('click', () => {
+  aside.classList.toggle('burger-out');
+  asideShedow.classList.toggle('visible');
+})
+openBurger.addEventListener('click', () => {
+  aside.classList.toggle('burger-out');
+  asideShedow.classList.toggle('visible');
 })
